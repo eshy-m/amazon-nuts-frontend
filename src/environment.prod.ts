f1671bef65b5f92e817d93cfd8d149541c906e08
eshy-m <ehuallpay@gmail.com>
2026-04-05 03:33:50 -0500

    actualizando

--- src/environment.prod.ts
+++ src/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://eshypro.com/api/public' // La dirección de Hostinger
+    apiUrl: 'https://eshypro.com/api' // La dirección de Hostinger
 };
