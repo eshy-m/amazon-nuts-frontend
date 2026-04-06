a8cb2568872b33dcbcd0dfa94cf053b262bb970c
eshy-m <ehuallpay@gmail.com>
2026-04-06 00:46:10 -0500

    actualizando frontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
     production: true,
     // Esta es la ruta de tu API real en internet
     apiUrl: 'https://eshypro.com/backend/public/api',
-    // Esta es la ruta donde Angular buscará las imágenes en internet
+    // Esta es la ruta donde Angular buscará las imágenes en indternet
     storageUrl: 'https://eshypro.com/backend/public/'
 
 };
