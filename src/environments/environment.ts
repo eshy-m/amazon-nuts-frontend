11a1bc802189c6afaec422ca16c552014e9a9860
eshy-m <ehuallpay@gmail.com>
2026-04-06 00:59:49 -0500

    actualizando frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
     production: false,
     // Esta es la ruta para hacer pruebas en tu computadora
     apiUrl: 'http://localhost:8000/api',
-    // Esta es la ruta donde Angular buscará las imágenes en tu computadora
+    // Esta es la ruta donde Angular buscará las imágsenes en tu computadora
     storageUrl: 'http://localhost:8000/storage/'
 };
